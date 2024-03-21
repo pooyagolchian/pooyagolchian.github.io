22f88f2e6e28349fd7359e48878d63fd2eb28526
Pooya Golchian <pooya.golchian@gmail.com>
2024-03-22 01:03:53 +0400

    ✨ 📝 Refactor: Remove duplicate getRandomColor function in Github component 🔄
    - Remove duplicate getRandomColor function in Github component to improve code
      readability and maintainability.

--- src/components/Github/index.tsx
+++ src/components/Github/index.tsx
@@ -3,6 +3,32 @@ import gsap from 'gsap'
 import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
 import data, { type Item } from './data'
 
+const getRandomColor = (): string => {
+  const colors = [
+    '#4b0082',
+    '#0000ff',
+    '#00ff00',
+    '#ffff00',
+    '#ff7f00',
+    '#ff0000',
+    '#800080',
+    '#ff1493',
+    '#ff4500',
+    '#7cfc00',
+    '#00ced1',
+    '#4682b4',
+    '#20b2aa',
+    '#f0e68c',
+    '#dda0dd',
+    '#00fa9a',
+    '#87cefa',
+    '#ba55d3',
+    '#ff69b4',
+    '#40e0d0',
+  ]
+  return colors[Math.floor(Math.random() * colors.length)]
+}
+
 const GitHub = () => {
   const containerRef = useRef<HTMLDivElement>(null)
 
@@ -54,32 +80,6 @@ const GitHub = () => {
       })}
     </div>
   )
-}
-
-const getRandomColor = (): string => {
-  const colors = [
-    '#4b0082',
-    '#0000ff',
-    '#00ff00',
-    '#ffff00',
-    '#ff7f00',
-    '#ff0000',
-    '#800080',
-    '#ff1493',
-    '#ff4500',
-    '#7cfc00',
-    '#00ced1',
-    '#4682b4',
-    '#20b2aa',
-    '#f0e68c',
-    '#dda0dd',
-    '#00fa9a',
-    '#87cefa',
-    '#ba55d3',
-    '#ff69b4',
-    '#40e0d0',
-  ]
-  return colors[Math.floor(Math.random() * colors.length)]
 }
 
 export default GitHub
