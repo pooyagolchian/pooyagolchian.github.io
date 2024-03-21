68d676ccb394afb5826d9e0972a33b5f94313855
Pooya Golchian <pooya.golchian@gmail.com>
2024-03-21 23:08:33 +0400

    Add Github action

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: 'https://pooya.blog',
+  base: 'pooyagolchian.github.io',
   integrations: [tailwind(), react()],
 });
